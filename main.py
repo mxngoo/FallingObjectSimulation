b5959a3f986de9a4c3e9640017208a3fb281ca21
mxngoo <j.kapadia2007@gmail.com>
2025-08-13 16:32:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from vpython import * 
-import math
+import math 
 
 
-ring(pos = vec(0,0,0), axis = vec(0,1,0), radius = math.pi + 5, thickness = 5, color = vector(1,0.65,1))
+ring(pos = vec(0, 0, 0), axis = vec(0, 1, 0), radius = math.pi + 5, thickness = 5, color = vector(1, 0.65, 1))
 
 m = 0.155
 r = 0.035 
-ball = sphere(pos = vector(0,500,0), radius = r, color = vector(0.49,0.49,1), make_trail = True) # ball is being dropped from 500m in the air
+ball = sphere(pos = vector(0, 500, 0), radius = r, color = vector(0.49, 0.49, 1), make_trail = True) # ball is being dropped from 500m in the air
 rho = 1.3 # density of air
 area = math.pi * r**2 # cross-sectional area of ball
 C = 0.35 # drag coefficent
 
-p = m * vector(0,9.81,0)
-gravity = m * vector(0,-9.81,0)
+p = m * vector(0, 9.81, 0)
+gravity = m * vector(0, -9.81, 0)
 t = 0
 delta_t = 0.01
 
-vertical_velocity = gcurve(color = vector(0.49,0.49,1))
+vertical_velocity = gcurve(color = vector(0.49, 0.49, 1))
 
 while ball.pos.y > 0.035:
     rate(100)
