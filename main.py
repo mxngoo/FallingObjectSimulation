fe89e0b41c03d5f46fac9c8b80e67cb81395f257
mxngoo <j.kapadia2007@gmail.com>
2025-08-19 18:38:16 +0100

    19.8.25

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from vpython import *
 import math 
 
 scene = canvas(title = "Falling Object Simulation", caption = "Adjust constants then click on the screen to play the simulation!")
+scene.append_to_caption("\n\n")
 
 ring(pos = vec(0, 0, 0), axis = vec(0, 1, 0), radius = math.pi + 5, thickness = 5, color = vector(1, 0.65, 1))
 
@@ -15,25 +16,59 @@ area = math.pi * r**2 # cross-sectional area of ball
 C = 0.35 # drag coefficent
 
 # Slider Functions:
-def set_radius(evt):
+def set_radius(rad):
     global r
-    r = evt.value
+    r = rad.value
     ball.radius = r
 
-def set_height(evt):
+def set_height(height):
     global h
-    h.y = evt.value
+    h.y = height.value
     ball.pos = h
+    
+def set_mass(mass):
+    global m
+    m = mass.value
+
+def set_rho(density):
+    global rho
+    rho = density.value
+
+def set_drag(drag):
+    global C
+    C = drag.value
     
 
 # Sliders with Labels:
 
+scene.append_to_caption("Radius of Ball (m): ")
+scene.append_to_caption("\n\n")
 radius_slider = slider(bind = set_radius, max = 0.1, min = 0.05, step = 0.01)
+scene.append_to_caption("\n\n")
 
+scene.append_to_caption("Initial Height (m): ")
+scene.append_to_caption("\n\n")
 height_slider = slider(bind = set_height, max = 500, min = 100, step = 1)
+scene.append_to_caption("\n\n")
+
+scene.append_to_caption("Mass of Ball (kg): ")
+scene.append_to_caption("\n\n")
+mass_slider = slider(bind = set_mass, max = 1, min = 0.01, step = 0.01)
+scene.append_to_caption("\n\n")
+
+scene.append_to_caption("Density of Fluid (kg/m^3)")
+scene.append_to_caption("\n\n")
+rho_slider = slider(bind = set_rho, max = 3, min = 0.1, step =  0.1)
+scene.append_to_caption("\n\n")
+
+scene.append_to_caption("Drag Coefficient")
+scene.append_to_caption("\n\n")
+drag_slider = slider(bind = set_drag, max = 0.45, min = 0.35, step = 0.01)
+scene.append_to_caption("\n\n")
 
 
-start_screen = scene.waitfor('click')   # Pauses screen on start-up
+# Start Screen
+start_screen = scene.waitfor('click')  
 ball.make_trail = True
 
 
